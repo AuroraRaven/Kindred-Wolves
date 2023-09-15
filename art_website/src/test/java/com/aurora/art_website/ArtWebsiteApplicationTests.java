10d138f29238f862a760d29c129c14562fec0eb9
AuroraRaven <125933407+AuroraRaven@users.noreply.github.com>
2023-09-15 14:47:57 -0500

    Add files via upload

---
+++ art_website/src/test/java/com/aurora/art_website/ArtWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.art_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
