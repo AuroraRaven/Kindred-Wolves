0acd65cb8ec65334c5ecf0d3403f4d231e58cf00
AuroraRaven <125933407+AuroraRaven@users.noreply.github.com>
2023-09-17 20:25:35 -0500

    prints page

---
+++ art_website/src/main/java/com/aurora/art_website/controllers/PrintsController.java
@@ -0,0 +1,17 @@
+package com.aurora.art_website.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/prints")
+public class PrintsController {
+    @GetMapping
+    public String displayPrintsPage(Model model){
+        model.addAttribute("title","Digital Art Prints");
+        return "prints";
+
+    }
+}
